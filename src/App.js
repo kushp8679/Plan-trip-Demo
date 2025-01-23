80841661979f02304beaa4d072675d742988ab33
kush <kushp8679@gmail.com>
2025-01-23 15:36:09 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -23,44 +23,29 @@ class App extends Component {
             color: "white",
           }}
         >
-          <div className="col-4">
-            
-                       <img  className="logo_lefttop" src="\Media\Frame (1).png"   alt='logo'/>
-         </div>
-          <div className="col-4 ">
-                      
+        
+          <div className="logo-container">
+  <div className="logo-left">
+    <img className="logo_lefttop" src="\Media\Frame (1).png" alt="logo" />
+  </div>
 
+  <div className="logo-center">
+    <img className="logo_big" src="\Media\Group 591.png" alt="logo" />
+    <img className="logo_person" src="\Media\Cutout.png" alt="logo" />
+  </div>
 
-                        
-                       <div className="container">
-                        <div className="row">
-                          <div className="col">
-                             <img  className="logo_big" src="\Media\Group 591.png"   alt='logo'/>
-                          
-                         
-                             <img  className="logo_person" src="\Media\Cutout.png"   alt='logo'/>
-                          </div>
-                        </div>
-                      </div>
+  <div className="logo-right">
+    <div className="logo-notification">
+      <img className="logo_not" src="\Media\notification-svgrepo-com 1 (1).png" alt="logo" />
+      <img className="logo_acc" src="\Media\Group.png" alt="logo" />
+    </div>
+    <div className="logo-component">
+      <img className="logo_com" src="/Media/Component 1.png" alt="logo" />
+    </div>
+  </div>
+</div>
 
-          
-          </div>
-          <div className="col-4 d-flex justify-content-center align-items-center">
-         
-           <div className="container">
-            <div className="row">
-              <div className="row-6">
-                              <img  className="logo_not" src="\Media\notification-svgrepo-com 1 (1).png"   alt='logo'/>
-                              <img  className="logo_acc" src="\Media\Group.png"   alt='logo'/>     
-              </div>
-              <div className="col-6">
-                              <img  className="logo_com" src="/Media/Component 1.png"   alt='logo'/>
-              </div>
-            </div>
-          </div>
-
-          
-          </div>
+        
         </div>
       </div>
     );
